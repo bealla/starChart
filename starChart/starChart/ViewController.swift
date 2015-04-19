5cead7e3548648f00ac5a978b5f5a48b07fdacfb
liltove <kringlan@uci.edu>
2015-04-18 19:49:50 -0700

    Revert 213a42b..55c11b9
    This rolls back to commit 213a42bba07dbb94d2a9bff0a408cb6dd3b38eba.

--- starChart/starChart/ViewController.swift
+++ starChart/starChart/ViewController.swift
@@ -10,139 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
-    @IBOutlet weak var background: UIImageView!
-    
-    @IBOutlet weak var appTitleBar: UIImageView!
-    
-    @IBOutlet weak var appLabel: UILabel!
-    
-    @IBOutlet weak var topBar: UIImageView!
-    
-    @IBOutlet weak var classesBar: UIImageView!
-    
-    @IBOutlet weak var starContainer: UIImageView!
-    
-    
-    @IBOutlet weak var behavior1: UILabel!
-    
-    @IBOutlet weak var behavior2: UILabel!
-    
-    @IBOutlet weak var behavior3: UILabel!
-    
-    
-    //@IBOutlet weak var b1s1: UIImageView!
-
-   
- 
-    
-   // @IBOutlet weak var b1s1: UIButton!
-  
- 
-
-    @IBOutlet weak var name: UITextField!
-    @IBOutlet weak var status: UILabel!
-    @IBOutlet weak var address: UITextField!
-    @IBOutlet weak var phone: UITextField!
-    
-    var databasePath = NSString()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-
-        
-           }
-  
-   
-    //@IBAction func b1s1pressed(sender: AnyObject) {
-      //  b1s1.image = UIImage(named: "starclicked.png")
-   // }
-
-        let filemgr = NSFileManager.defaultManager()
-        let dirPaths =
-        NSSearchPathForDirectoriesInDomains(.DocumentDirectory,
-            .UserDomainMask, true)
-        
-        let docsDir = dirPaths[0] as String
-        
-        databasePath = docsDir.stringByAppendingPathComponent(
-        "contacts.db")
-        
-        if !filemgr.fileExistsAtPath(databasePath as String) {
-            
-            let contactDB = FMDatabase(path: databasePath as String)
-            
-            if contactDB == nil {
-                println("Error: \(contactDB.lastErrorMessage())")
-            }
-            
-            if contactDB.open() {
-                let sql_stmt = "CREATE TABLE IF NOT EXISTS STUDENT (ID INTEGER PRIMARY KEY AUTOINCREMENT, SID MEDIUMINT NOT NULL AUTO_INCREMENT, NAME TEXT); CREATE TABLE IF NOT EXISTS CLASS (ID INTEGER PRIMARY KEY AUTOINCREMENT, CID MEDIUMINT NOT NULL AUTO_INCREMENT, NAME TEXT, BEGIN DATE, END DATE); CREATE TABLE IF NOT EXISTS STUDENTCLASS (ID INTEGER PRIMARY KEY AUTOINCREMENT, SID MEDIUMINT, CID MEDIUMINT, PERIOD INTEGER); REATE TABLE IF NOT EXISTS BEHAVIOR (ID INTEGER PRIMARY KEY AUTOINCREMENT, BID MEDIUMINT NOT NULL AUTO_INCREMENT, NAME TEXT); CREATE TABLE IF NOT EXISTS STUDENTBEHAVIOR (ID INTEGER PRIMARY KEY AUTOINCREMENT, SID MEDIUMINT, CID MEDIUMINT, BID MEDIUMINT, STARS INTEGER, DATELOGGED DATETIME); CREATE TABLE IF NOT EXISTS COMMENTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, SID MEDIUMINT, PERIOD INTEGER, COMMENT TEXT, DATELOGGED DATETIME)"
-                if !contactDB.executeStatements(sql_stmt) {
-                    println("Error: \(contactDB.lastErrorMessage())")
-                }
-                contactDB.close()
-            } else {
-                println("Error: \(contactDB.lastErrorMessage())")
-            }
-        }
-        
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func saveData(sender: AnyObject) {
-        let contactDB = FMDatabase(path: databasePath as String)
-        
-        if contactDB.open() {
-            
-            let insertSQL = "INSERT INTO CONTACTS (name, address, phone) VALUES ('\(name.text)', '\(address.text)', '\(phone.text)')"
-            
-            let result = contactDB.executeUpdate(insertSQL,
-                withArgumentsInArray: nil)
-            
-            if !result {
-                status.text = "Failed to add contact"
-                println("Error: \(contactDB.lastErrorMessage())")
-            } else {
-                status.text = "Contact Added"
-                name.text = ""
-                address.text = ""
-                phone.text = ""
-            }
-        } else {
-            println("Error: \(contactDB.lastErrorMessage())")
-        }
-    }
-    
-    @IBAction func findContact(sender: AnyObject) {
-        let contactDB = FMDatabase(path: databasePath as String)
-        
-        if contactDB.open() {
-            let querySQL = "SELECT address, phone FROM CONTACTS WHERE name = '\(name.text)'"
-            
-            let results:FMResultSet? = contactDB.executeQuery(querySQL,
-                withArgumentsInArray: nil)
-            
-            if results?.next() == true {
-                address.text = results?.stringForColumn("address")
-                phone.text = results?.stringForColumn("phone")
-                status.text = "Record Found"
-            } else {
-                status.text = "Record not found"
-                address.text = ""
-                phone.text = ""
-            }
-            contactDB.close()
-        } else {
-            println("Error: \(contactDB.lastErrorMessage())")
-        }
-    }
-    
 
+}
 
