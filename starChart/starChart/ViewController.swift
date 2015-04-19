3d7ff57d8dbad693ff335bdc6e3c649e3034acf4
bealla <alexandraabeall@gmail.com>
2015-04-19 12:34:49 -0700

    Revert b3e7e60..525b642
    This rolls back to commit b3e7e60f23a8380c819cf0b536611f9f3b725300.

--- starChart/starChart/ViewController.swift
+++ starChart/starChart/ViewController.swift
@@ -10,45 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var background: UIImageView!
-    
-    @IBOutlet weak var appTitleBar: UIImageView!
-    
-    @IBOutlet weak var appLabel: UILabel!
-    
-    @IBOutlet weak var topBar: UIImageView!
-    
-    @IBOutlet weak var classesBar: UIImageView!
-    
-    @IBOutlet weak var starContainer: UIImageView!
-    
-    
-    @IBOutlet weak var behavior1: UILabel!
-    
-    @IBOutlet weak var behavior2: UILabel!
-    
-    @IBOutlet weak var behavior3: UILabel!
-    
-    
-    //@IBOutlet weak var b1s1: UIImageView!
-
-   
- 
-    
-   // @IBOutlet weak var b1s1: UIButton!
-  
- 
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-           }
-  
-   
-    //@IBAction func b1s1pressed(sender: AnyObject) {
-      //  b1s1.image = UIImage(named: "starclicked.png")
-   // }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
