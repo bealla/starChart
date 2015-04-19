787ebdcf8975518b5ea739071cc211c7dda367c7
bealla <alexandraabeall@gmail.com>
2015-04-19 14:27:22 -0700

    extremely basic UI layout, no color yet

--- starChart/starChart/ViewController.swift
+++ starChart/starChart/ViewController.swift
@@ -10,10 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    
+    @IBOutlet weak var titlebar: UIImageView!
+    
+    @IBOutlet weak var studentname: UIImageView!
+    
+    @IBOutlet weak var classes: UIImageView!
+    
+    
+    @IBOutlet weak var starbarContainer: UIImageView!
+    
+    
+    @IBOutlet weak var behavior1Label: UILabel!
+    
+    
+    @IBOutlet weak var behavior2Label: UILabel!
+    
+    @IBOutlet weak var behavior3Label: UILabel!
+    
+    @IBOutlet weak var appTitle: UILabel!
+    
+    
+    @IBOutlet weak var b1s1: UIImageView!
+    
+    @IBAction func b1s1button(sender: AnyObject) {
+      //  b1s1.image = UIImageView(named: "starclicked.png")
+    }
+    
+  
+    
+    @IBAction func hamButton(sender: AnyObject) {
+    }
+    
+    @IBAction func viewStudents(sender: AnyObject) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
