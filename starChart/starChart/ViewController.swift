41e32b11d209106bd1e01a9e6c3086d9f1fa035c
bealla <alexandraabeall@gmail.com>
2015-04-20 10:57:19 -0700

    most of layout and app flow

--- starChart/starChart/ViewController.swift
+++ starChart/starChart/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPopoverPresentationControllerDelegate{
 
     @IBOutlet weak var background: UIImageView!
     
@@ -31,13 +31,27 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var appTitle: UILabel!
     
-    
+    //star coin images for each behavior
     @IBOutlet weak var b1s1: UIImageView!
-    
-    
     @IBOutlet weak var b1s2: UIImageView!
     @IBOutlet weak var b1s3: UIImageView!
     
+    @IBOutlet weak var b2s1: UIImageView!
+    @IBOutlet weak var b2s2: UIImageView!
+    @IBOutlet weak var b2s3: UIImageView!
+    
+    @IBOutlet weak var b3s1: UIImageView!
+    @IBOutlet weak var b3s2: UIImageView!
+    @IBOutlet weak var b3s3: UIImageView!
+    
+    //parts of rainbow star
+    
+    @IBOutlet weak var redChunk: UIImageView!
+    @IBOutlet weak var yellowChunk: UIImageView!
+    @IBOutlet weak var greenChunk: UIImageView!
+    @IBOutlet weak var rainbowChunk: UIImageView!
+    
+    //behavoir one stars buttons
     @IBAction func b1s1button(sender: AnyObject) {
         b1s1.image = UIImage(named: ("starclicked.png"))
         b1s2.image = UIImage(named: ("unclickedStar.png"))
@@ -57,12 +71,62 @@ class ViewController: UIViewController {
         b1s3.image = UIImage(named: ("starclicked.png"))
     }
     
-    @IBAction func hamButton(sender: AnyObject) {
+    //behavior two star buttons
+   
+    @IBAction func b2s1button(sender: AnyObject) {
+        b2s1.image = UIImage(named: ("starclicked.png"))
+        b2s2.image = UIImage(named: ("unclickedStar.png"))
+        b2s3.image = UIImage(named: ("unclickedStar.png"))
     }
     
+    @IBAction func b2s2Button(sender: AnyObject) {
+        b2s1.image = UIImage(named: ("starclicked.png"))
+        b2s2.image = UIImage(named: ("starclicked.png"))
+        b2s3.image = UIImage(named: ("unclickedStar.png"))
+    }
+    
+    
+    @IBAction func b2s3Button(sender: AnyObject) {
+        b2s1.image = UIImage(named: ("starclicked.png"))
+        b2s2.image = UIImage(named: ("starclicked.png"))
+        b2s3.image = UIImage(named: ("starclicked.png"))
+    }
+    
+    //behavior three star buttons
+    
+    @IBAction func b3s1(sender: AnyObject) {
+        b3s1.image = UIImage(named: ("starclicked.png"))
+        b3s2.image = UIImage(named: ("unclickedStar.png"))
+        b3s3.image = UIImage(named: ("unclickedStar.png"))
+    }
+    
+    @IBAction func b3s2(sender: AnyObject) {
+        b3s1.image = UIImage(named: ("starclicked.png"))
+        b3s2.image = UIImage(named: ("starclicked.png"))
+        b3s3.image = UIImage(named: ("unclickedStar.png"))
+    }
+    @IBAction func b3s3(sender: AnyObject) {
+        b3s1.image = UIImage(named: ("starclicked.png"))
+        b3s2.image = UIImage(named: ("starclicked.png"))
+        b3s3.image = UIImage(named: ("starclicked.png"))
+    }
+    
+    //function to fill up star tail as more stars are added, do an if statement setting each color chunk to its color if x amonut of stars have been collected, SQL
+    func fillTail(){
+        
+    }
+    
+    
+    
+   
+    //set text to whichever student's being view name, SQL
     @IBAction func viewStudents(sender: AnyObject) {
+        
+        
     }
     
+    @IBAction func hamburgerButton(sender: AnyObject) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -72,6 +136,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func adaptivePresentationStyleForPresentationController(controller: UIPresentationController!, traitCollection: UITraitCollection!) -> UIModalPresentationStyle {
+        return .None
     }
 
 
