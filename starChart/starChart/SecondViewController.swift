83c43dfef5b53c13294e983e9d70e01f27b5750f
liltove <kringlan@uci.edu>
2015-04-20 18:40:18 -0700

    Added code from Alex main page
    Main page buttons with functions

---
+++ starChart/starChart/SecondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.swift
+//  starChart
+//
+//  Created by Alexandra Beall on 4/19/15.
+//  Copyright (c) 2015 Alexandra Beall. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    
+    @IBOutlet weak var listStudentsTable: UITableView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ starChart/starChart/listofClassesViewController.swift
@@ -0,0 +1,34 @@
+//
+//  listofClassesViewController.swift
+//  starChart
+//
+//  Created by Alexandra Beall on 4/20/15.
+//  Copyright (c) 2015 Alexandra Beall. All rights reserved.
+//
+
+import UIKit
+
+class listofClassesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
